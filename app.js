4cf2c9ae367a33367df9f31340e58a32a68425e7
Michael Seid <mbseid@gmail.com>
2015-06-14 22:56:45 -0700

    First pass of the RT & Bri wedding web page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var combo   = require('combohandler'),
 
 // -- Configure ----------------------------------------------------------------
 
-app.set('name', 'Leslie-Eric Wedding');
+app.set('name', 'Bri&RT Wedding');
 app.set('env', config.env);
 app.set('port', config.port);
 app.set('views', config.dirs.views);
@@ -33,8 +33,8 @@ app.engine('hbs', exphbs({
 app.expose(config.yui.config, 'window.YUI_config');
 
 app.locals({
-    title   : 'Leslie & Eric',
-    appTitle: 'L&E Wedding',
+    title   : 'Brianna & RT',
+    appTitle: 'B&RT Wedding',
 
     version    : config.version,
     yui_version: config.yui.version,
